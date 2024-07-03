8104a3af52c33603248f09ef8556aba91e485753
RARRIT 94 <94345781+rarrit@users.noreply.github.com>
2024-07-03 16:42:58 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,8 +1,12 @@
-
-const solution = angle => {
-    if (angle > 0 && angle < 90) return 1;
-    else if(angle === 90) return 2;
-    else if(angle < 180) return 3;
-    else return 4;
+// 첫번째수정
+const solution = angle => {    
+    if(angle > 0 && angle < 90) {
+        return 1;
+    }else if(angle === 90) {
+        return 2;   
+    }else if(angle > 90 && angle < 180){
+        return 3;
+    }else if(angle == 180){
+        return 4;
+    }
 }
-
