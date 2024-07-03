b03f63002735dc8d8214b72a731ed0fd0d311cd0
RARRIT 94 <94345781+rarrit@users.noreply.github.com>
2024-07-03 13:32:32 +0900

    [level 0] Title: 나머지 구하기, Time: 0.01 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.js
@@ -0,0 +1,10 @@
+// function solution(num1, num2) {
+//     var answer = -1;
+//     return answer;
+// }
+
+
+const solution = (num1, num2) => num1 % num2
+
+console.log(solution(3, 2));
+console.log(solution(10,5));
