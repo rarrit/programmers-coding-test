9caf09048202815ea108ac4176c7cfe08d4abc52
RARRIT 94 <94345781+rarrit@users.noreply.github.com>
2024-07-03 17:04:37 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,12 +1,1 @@
-function solution(num1, num2) {
-    let answer = num1 == num2 ? 1 : -1;
-    return answer;
-}
-// 입출력 예 설명 #1
-console.log(2, 3);
-
-// 입출력 예 설명 #2
-console.log(11, 11);
-
-// 입출력 예 설명 #3
-console.log(7, 99);
+const solution = (num1, num2) => num1 == num2 ? 1 : -1;
