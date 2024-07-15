8e0a65253e253a76e6c0460299b6f094d77e8b87
RARRIT 94 <94345781+rarrit@users.noreply.github.com>
2024-07-15 21:11:26 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 5.57 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,7 +1,5 @@
 const solution = n => {
-    let arr = [];
-    for(let i = 1; i <= n; i++){
-        if(n % i === 1) arr.push(i);
-    }
-    return arr.reduce((a,b) => a < b ? parseInt(a) : parseInt(b));
+  for(let i = 0; i <= n; i ++) {
+    if(n % i === 1) return i;
+  }
 }
