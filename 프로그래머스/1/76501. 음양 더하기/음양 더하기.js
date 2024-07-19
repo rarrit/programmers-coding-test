25298be795a6b681dee1487a9bf390a6e3a6b9cb
RARRIT 94 <94345781+rarrit@users.noreply.github.com>
2024-07-19 10:57:44 +0900

    [level 1] Title: 음양 더하기, Time: 0.24 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,8 @@
+const solution = (absolutes, sings) => {
+    for(let i = 0; i < absolutes.length; i ++) {
+        if(sings[i] === true) absolutes[i] = Math.abs(absolutes[i]) 
+        else absolutes[i] = (absolutes[i]) * -1;
+    }
+    return absolutes.reduce((a, b) => a + b, 0);
+}
+console.log(solution([4,7,12],[true,false,true]));
