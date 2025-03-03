5a69de639d950f8a28320db09fecac0812e2b321
Minkyu Kim <94345781+rarrit@users.noreply.github.com>
2025-03-03 23:11:13 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,12 @@
+
+const solution = (my_string, n) => {
+    let answer = '';
+    
+    for(let i = 0; i < my_string.length; i++){
+        for (let j = 0; j < n; j++){
+            answer += my_string[i]
+        }
+    }
+    
+    return answer;
+}
