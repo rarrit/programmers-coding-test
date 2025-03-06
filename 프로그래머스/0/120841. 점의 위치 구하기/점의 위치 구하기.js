e7e22122abd767310d20648377df6bffd7053876
Minkyu Kim <94345781+rarrit@users.noreply.github.com>
2025-03-06 11:01:09 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,7 @@
+const solution = dot => {
+    if(dot[0] > 0 && dot[1] > 0) return 1
+    else if(dot[0] < 0 && dot[1] > 0) return 2
+    else if(dot[0] < 0 && dot[1] < 0) return 3
+    else if(dot[0] > 0 && dot[1] < 0) return 4
+    else return "값이 뭐임"
+}
