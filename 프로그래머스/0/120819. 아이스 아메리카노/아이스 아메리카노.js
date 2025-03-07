3ca7b51eeff67e358c51b86a4a5525173e159f77
Minkyu Kim <94345781+rarrit@users.noreply.github.com>
2025-03-07 09:22:19 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -0,0 +1,10 @@
+const solution = (money) => {
+   const americano = 5500;
+    
+    const glass = Math.floor(money / americano);
+    const change = money % americano;
+    
+    const ans = [glass, change];
+    
+    return ans;
+}
